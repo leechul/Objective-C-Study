fb3bedebc6cb9caf63a7e5000167afa9d65e00f8
LeeChul <leechul@me.com>
2014-08-15 15:36:21 +0900

    first

---
+++ CarParts/CarParts/mainCarParts.m
@@ -0,0 +1,76 @@
+#import <Foundation/Foundation.h>
+
+//Tire
+@interface Tire : NSObject
+@end// Tire
+
+
+@implementation Tire
+
+- (NSString *) description
+{
+    return (@"I am a tire. I last a while");
+}// description
+
+@end// Tire
+
+
+//Engine
+@interface Engine : NSObject
+@end
+
+@implementation Engine
+
+- (NSString *) description
+{
+    return (@"I am an Engine. Vrooom!");
+}
+@end
+
+
+//Car
+
+@interface Car : NSObject
+{
+    Engine  *engine;
+    Tire    *tires[4];
+}
+
+- (void)    print;
+
+@end
+
+@implementation Car
+
+- (id)  init
+{
+    if (self = [super init]){
+        engine = [Engine new];
+        tires[0] = [Tire new];
+        tires[1] = [Tire new];
+        tires[2] = [Tire new];
+        tires[3] = [Tire new];
+    }
+    return (self);
+}
+
+- (void) print
+{
+    NSLog(@"%@", engine);
+    
+    NSLog(@"%@", tires[0]);
+    NSLog(@"%@", tires[1]);
+    NSLog(@"%@", tires[2]);
+    NSLog(@"%@", tires[3]);
+}
+@end
+
+int main ( int argc, const char * argv[])
+{
+    Car *car;
+    
+    car = [Car new];
+    [car print];
+    
+    return (0);
+}
