a7e26fff3d2af69de9091eec0a9ee448bc9a959b
LeeChul <leechul@me.com>
2014-08-15 15:05:09 +0900

    CarParts

---
+++ CarParts/CarParts/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CarParts
+//
+//  Created by Chul Lee on 2014. 8. 15..
+//  Copyright (c) 2014년 ipageon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
